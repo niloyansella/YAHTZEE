e371ab790fe3f03a218577053facac8ad15077d1
niloyansella <74381680+niloyansella@users.noreply.github.com>
2022-02-14 21:51:38 +0100

    update

--- vendor/app.js
+++ vendor/app.js
@@ -1,11 +1,18 @@
 const rollDice = document.getElementById("roll-dice");
-const dices = document.querySelector(".dice1");
+const firstDice = document.querySelector(".dice1");
+const secondDice = document.querySelector(".dice2");
+const thirdDice = document.querySelector(".dice3");
+const fourthDice = document.querySelector(".dice4");
+const fifthDice = document.querySelector(".dice5");
 
-console.log(dices);
+const toggleButton = () => {
+  firstDice.classList.toggle("rollDice");
+  secondDice.classList.toggle("rollDice");
+  thirdDice.classList.toggle("rollDice");
+  fourthDice.classList.toggle("rollDice");
+  fifthDice.classList.toggle("rollDice");
+};
+
 rollDice.addEventListener("click", () => {
-  dices.classList.add("rollDice");
-});
-
-rollDice.addEventListener("animationend", () => {
-  dices.classList.remove("rollDice");
+  toggleButton();
 });
