61a233e274a2fa3a39b682c49a829e22bdff9f6c
niloyansella <74381680+niloyansella@users.noreply.github.com>
2022-02-14 23:25:04 +0100

    update

--- vendor/app.js
+++ vendor/app.js
@@ -15,4 +15,20 @@ const toggleButton = () => {
 
 rollDice.addEventListener("click", () => {
   toggleButton();
+  getRandomDice();
 });
+
+const getRandomDice = () => {
+  const randomvalue = Math.random();
+  if (randomvalue < 0.2) {
+    return firstDice;
+  } else if (randomvalue < 0.4) {
+    console.log(secondDice);
+  } else if (randomvalue < 0.6) {
+    console.log(thirdDice);
+  } else if (randomvalue < 0.8) {
+    console.log(fourthDice);
+  } else if (randomvalue < 1) {
+    console.log(fifthDice);
+  }
+};
